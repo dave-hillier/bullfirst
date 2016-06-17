92d6f2fb057086a816b541a6dff00565b84aca0c
David Hillier <daveh@Davids-MacBook-Pro.local>
2016-06-17 11:14:49 +0100

    Moved data into separate storage and added watch

--- AccountsController.js
+++ AccountsController.js
@@ -1,62 +1,6 @@
-bullsfirstModule.controller('AccountsController', function($scope){
+bullsfirstModule.controller('AccountsController', function($scope, accountsStorage){
 
-    // TODO: Data should come from service
-    $scope.rows = [
-        {
-            name: 'Brokerage Account 3',
-            marketValue: 1999990,
-            cash: 1995826,
-            legend: 'orange'
-        },
-        {
-            name: 'Account 3',
-            marketValue: 1949990,
-            cash: 1695856,
-            legend: 'darkorange'
-        },
-        {
-            name: 'Brokerage Account 1',
-            marketValue: 1349990,
-            cash: 1595866,
-            legend: 'red'
-        },
-        {
-            name: 'Brokerage Account 4',
-            marketValue: 155990,
-            cash: 160826,
-            legend: 'blue'
-        },
-        {
-            name: 'Brokerage Account 2',
-            marketValue: 74560,
-            cash: 19956,
-            legend: 'gray'
-        },
-        {
-            name: 'Account 4',
-            marketValue: 55006,
-            cash: 53006,
-            legend: 'salmon'
-        },
-        {
-            name: 'Account 13',
-            marketValue: 37340,
-            cash: 0,
-            legend: 'green'
-        },
-        {
-            name: 'Joint Account 1',
-            marketValue: 28308,
-            cash: 4167,
-            legend: 'darkblue'
-        },
-        {
-            name: 'Joint Account 2',
-            marketValue: 10000,
-            cash: 10000,
-            legend: 'teal'
-        }
-    ];
+    var rows = $scope.rows = accountsStorage.get();
 
     $scope.totals = {
         name: '',
@@ -65,15 +9,17 @@ bullsfirstModule.controller('AccountsController', function($scope){
         legend: 'none'
     };
 
-    // TODO: Not recalculated.
-    for (var i = 0; i < $scope.rows.length; i++) {
-        $scope.totals.marketValue += $scope.rows[i].marketValue;
-        $scope.totals.cash += $scope.rows[i].cash;
-    }
-
+	$scope.$watch('rows', function () {
+        console.log('watch');
+        for (var i = 0; i < rows.length; i++) {
+            $scope.totals.marketValue += rows[i].marketValue;
+            $scope.totals.cash += rows[i].cash;
+        }
+        accountsStorage.put(rows);
+    }, true);
 
     $scope.addAccount = function() {
-        $scope.rows.push({
+        rows.push({
             name: 'New Account',
             marketValue: Math.random() * 100000,
             cash: Math.random() * 400000,
